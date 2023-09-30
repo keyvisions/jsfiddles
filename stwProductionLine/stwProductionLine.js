26835057a559d27f033c375eaf27238c0c94691c
Giancarlo Trevisan <g.trevisan@keyvisions.it>
2023-09-30 10:27:46 +0200

    Update stwProductionLine.js

--- stwProductionLine/stwProductionLine.js
+++ stwProductionLine/stwProductionLine.js
@@ -1,43 +1,36 @@
-const item = {
-  "code": "",
-  components": [{
-      "code": "aaa",
-      "description": "bla",
-      "udc": 7
-    },
-    {
-      "code": "bbb",
-      "description": "bla",
-      "udc": 3
-    }, {
-      "code": "ccc",
-      "description": "bla",
-      "udc": 7
-    }
-  ]
-}
-
 window.addEventListener('load', event => {
-  document.querySelectorAll('[json="stwProductionLine"]').forEach(table => {
-    const UDC = parseInt(table.dataset.udc); // Unità di carico
+  document.querySelectorAll('[data-json="stwProductionLine"]').forEach(widget => {
+  	const data = JSON.parse(widget.value);
+  
+		widget.insertAdjacentHTML('afterend', `
+    	<table>
+      <caption>${data.code}</caption>
+    	<thead><tr><th>Componente</th><th>Descrizione</th></tr></thead>
+      <tbody></tbody>
+      <tfoot></tfoot>
+      </table>`);
+  	widget.style.display = 'none';
+    const table = widget.nextElementSibling;
+
+    const UDC = data.udc; // Unità di carico presenti in linea
 
     let tr = '';
     for (let i = 1; i <= UDC; ++i)
       tr += `<th>UDC<sub>${i}</sub></th>`;
     table.querySelector('thead > tr').insertAdjacentHTML('beforeend', tr);
 
-    item.components.forEach((component, i) => {
+    data.components.forEach((component, i) => {
       tr = `<tr><td>${component.code}</td><td>${component.description}</td>${'<td><i class="fa-regular fa-fw fa-circle"></i></td>'.repeat(UDC)}</tr>`;
       table.querySelector('tbody').insertAdjacentHTML('beforeend', tr);
       table.querySelectorAll('tbody tr:last-child i')[component.udc].classList.replace('fa-regular', 'fa-solid');
     });
 
-    tr = `<tr><td colspan="2"></td>${'<th></th>'.repeat(UDC)}</tr>`;
+    tr = `<tr><td colspan="2">Totale componenti</td>${'<th></th>'.repeat(UDC)}</tr>`;
     table.querySelector('tfoot').insertAdjacentHTML('afterbegin', tr);
 
     table.tally = function () {
       let udc_c = Array(UDC);
-      item.components.forEach(component => {
+      data.components.forEach(component => {
         udc_c[component.udc] = ++udc_c[component.udc] || 1;
       });
       table.querySelectorAll('tfoot > tr th').forEach((th, i) => th.innerHTML = typeof udc_c[i] == 'undefined' ? '' : udc_c[i]);
@@ -51,8 +44,9 @@ window.addEventListener('load', event => {
         target.closest('tr').querySelectorAll('i.fa-circle').forEach((udc, i) => {
           udc.className = `fa-${udc == target && !udc.classList.contains('fa-solid') ? 'solid' : 'regular'} fa-fw fa-circle`;
           if (udc.className.indexOf('solid') != -1)
-            item.components.find(component => component.code == target.closest('tr').firstChild.innerText).udc = i;
+            data.components.find(component => component.code == target.closest('tr').firstChild.innerText).udc = i;
         });
+
         table.tally();
       }
     });
