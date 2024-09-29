5b1c4800fbb4b7fcd3c95c9b2b8ff0dadd60187a
Giancarlo Trevisan <g.trevisan@keyvisions.it>
2024-09-29 06:10:08 +0200

    Removed kv-budget

--- kv-gauge/kv-gauge.js
+++ kv-gauge/kv-gauge.js
@@ -35,7 +35,7 @@ class kvGauge extends HTMLElement {
         this.style.display = 'inline-block';
         this.innerHTML = `<svg width="${this.getAttribute("width") || 250}" height="${this.getAttribute("height") || 150}" viewBox="0 0 250 150">
             <g transform="translate(125 125) scale(0.75 0.75)">
-                <text class="title" y="-135">${this.getAttribute('title')}</text>
+                <text class="title" y="-135">${this.getAttribute('title') || ''}</text>
                 <path d="m -100 0 A 100 100 0 0 1 -81 -59" fill="transparent" stroke="red" stroke-width="50" />
                 <path d="m -81 -59 A 100 100 0 0 1 -31 -96" fill="transparent" stroke="orange" stroke-width="50" />
                 <path d="m -31 -96 A 100 100 0 0 1 30 -96" fill="transparent" stroke="yellow" stroke-width="50" />
@@ -52,12 +52,12 @@ class kvGauge extends HTMLElement {
     drawLinearGauge() {
         const values = this.getAttribute('values').split(';').map(value => parseFloat(value.replace(/,/g, '.')));
 
-        if (values.length !== 5) {
+        if (values.length === 5) {
             values.push(Math.max(values[3], values[0] + values[1] + values[2]) * 1.2);
 
             this.innerHTML = `
                 <div style="display:flex">
-                    <div style="width:${values[3] / values[5] * 100}%; border-left: thin solid black; border-right:thin solid black; padding-left:0.25em">${this.getAttribute('title')}<span style="float:right; padding-right:0.25em">Target</span></div>
+                    <div style="width:${values[3] / values[5] * 100}%; border-left: thin solid black; padding-left:0.25em">${this.getAttribute('title') || 'Budget'}<span style="float:right; width:${values[4] / values[5] * 100}%; background:linear-gradient(to right,lightblue,blue); border-left:thin solid lightblue">&nbsp;</span><span style="float:right; padding-right:0.25em">Target</span></div>
                   <div style="width:${(1.0 - values[3] / values[5]) * 100}%; text-align:right; border-right: thin solid black; padding-right: 0.25em;">&nbsp;</div>
                 </div>
                 <div style="display:flex; border-left: thin solid black; border-bottom:thin solid black; padding-bottom: 2px; border-right: thin solid black;">
@@ -69,7 +69,8 @@ class kvGauge extends HTMLElement {
                     <div style="width:${values[2] / values[5] * 100}%; height: 0.5em; background-color:yellow" title="Backlog € ${values[2].toLocaleString()}"></div>
                 </div>`;
         } else {
-            this.innerHTML = 'usage: Invoiced;Undelivered;Backlog;Budget;Wished';
+            this.innerHTML = '';
+            console.log('usage: Invoiced;Undelivered;Backlog;Budget;Wished');
         }
     }
 }
